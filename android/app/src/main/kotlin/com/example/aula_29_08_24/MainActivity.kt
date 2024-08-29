a8ee93cd35235f0585b1b7c93594bb0f43e827dd
Glebsonfernandes <comercialongold@gmail.com>
2024-08-29 20:45:57 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/aula_29_08_24/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aula_29_08_24
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:aula_29_08_24/app_widget.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const AppWidget());
+}
